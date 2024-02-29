ec26a0e2eed6552bb5493c3648091342b1f96959
Claudie <112946031+claudiacarter@users.noreply.github.com>
2024-02-29 22:12:16 +0000

    Update fastasplitter.py
    User input for sequences per file with input error checks

--- fastasplitter.py
+++ fastasplitter.py
@@ -12,7 +12,14 @@ with open(r"FILE-PATH-TO-FASTA", "r") as fasta:
         print("ERROR: Input file does not seem to be a FASTA.")
         exit()
 
-    lines_per_file = 50
+    #set how many sequences you want per file, lines will be double this in FASTA format
+    seqs_per_file = input(f"You must choose the number of sequences you would like in each file. If the number of \nsequences in the input file is not divisible by this number, the final file will contain \nfewer.\n\nEnter input here: ")
+    while seqs_per_file.isnumeric() == False :
+        seqs_per_file = input(f"\nInvalid input. Please enter an integer ONLY and hit Enter. Fastasplitter will split your\ninput FASTA into smaller files with this many sequences or less.\n\nEnter number here: ")
+    while int(seqs_per_file) > len(deflines):
+        seqs_per_file = input(f"You have requested more sequences per file than exist in the input file, please check \ninput and retry.\n\nEnter number here: ")
+    lines_per_file = 2*seqs_per_file
+    
     new_file_num = 0
     current_file = []
     count = 0
@@ -30,4 +37,4 @@ with open(r"FILE-PATH-TO-FASTA", "r") as fasta:
         current_file = []
         count = 0
 
-print(f"Split input FASTA to create {new_file_num} FASTAs, each contain {int(lines_per_file/2)} sequences or less.")
+print(f"Split input FASTA to create {new_file_num} FASTAs, each contain {seqs_per_file} sequences or less.")
